5951c43c5404caec210a6a468486df9fa9c073ff
Anatoly Duzenko <a.duzenko@wsmintl.com>
2021-01-14 13:54:20 -0500

    Init

---
+++ xlsx-parse.php
@@ -0,0 +1,8 @@
+/**
+ * Plugin Name: Anatoly's Xlsx Parser
+ * Version: 1.0.0
+ * Author: Anatoly Duzenko
+ * Author URI: www.hmmnuok.ru
+ * Description: This Plugin used to parse Excel files and convert them into appropriate format (or something else)
+ */
+
