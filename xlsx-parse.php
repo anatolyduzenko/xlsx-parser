57009f6d63d67f0f2859e82a58e75c8929c6e646
Anatoly Duzenko <a.duzenko@wsmintl.com>
2021-01-14 14:43:23 -0500

    Tweaks

--- xlsx-parse.php
+++ xlsx-parse.php
@@ -6,3 +6,16 @@
  * Description: This Plugin used to parse Excel files and convert them into appropriate format (or something else)
  */
 
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+require plugin_dir_path( __FILE__ ) . 'includes/ADXlsxParser.php';
+
+function run_plugin_name() {
+
+	$plugin = new ADXlsxParser();
+	$plugin->run();
+
+}
+run_plugin_name();
